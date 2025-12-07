bd95e63fd27173585422c1444c136bf0d2615d54
Samuel Ayomide Makanjuola <samuelifeoluwa111@gmail.com>
2025-12-07 09:55:46 +0100

    Update ddl_silver.sql

--- script/silver/ddl_silver.sql
+++ script/silver/ddl_silver.sql
@@ -13,16 +13,18 @@ IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info; 
  CREATE TABLE silver.crm_cust_info 
  (
-	 cst_id				      INT,
-	 cst_key			      NVARCHAR (50),
-	 cst_firstname		  NVARCHAR (50),
+	 cst_id				    INT,
+	 cst_key			    NVARCHAR (50),
+	 cst_firstname		  	NVARCHAR (50),
 	 cst_lastname		    NVARCHAR (50),
-	 cst_marital_status NVARCHAR (50),
-	 cst_gndr			      NVARCHAR (50),
-	 cst_create_date	  DATE,
-	 dwh_create_date	  DATETIME2 DEFAULT GETDATE()
+	 cst_marital_status 	NVARCHAR (50),
+	 cst_gndr			  	NVARCHAR (50),
+	 cst_create_date	  	DATE,
+	 dwh_create_date	  	DATETIME2 DEFAULT GETDATE()
  );
 
+GO 
+	
  IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
  CREATE TABLE silver.crm_prd_info
@@ -38,6 +40,8 @@ IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	 dwh_create_date DATETIME2 DEFAULT GETDATE()
  );
 
+GO
+	
  IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
  CREATE TABLE silver.crm_sales_details
@@ -54,13 +58,15 @@ IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	 dwh_create_date	DATETIME2 DEFAULT GETDATE()
  );
 
+GO
+	
  IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12; 
  CREATE TABLE silver.erp_cust_az12
  (
 	 cid	NVARCHAR(50),
 	 bdate	DATE,
-	 gen	  NVARCHAR(50),
+	 gen	NVARCHAR(50),
 	 dwh_create_date	DATETIME2 DEFAULT GETDATE()
  );
 
@@ -73,13 +79,17 @@ IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	dwh_create_date	DATETIME2 DEFAULT GETDATE()
 );
 
+GO 
+	
 IF OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 DROP TABLE silver.erp_px_cat_g1v2;
 CREATE TABLE silver.erp_px_cat_g1v2
 (
-	id			    NVARCHAR(50),
-	cat			    NVARCHAR(50),
-	subcat		  NVARCHAR(50),
+	id			NVARCHAR(50),
+	cat			NVARCHAR(50),
+	subcat		NVARCHAR(50),
 	maintenance NVARCHAR(50),
 	dwh_create_date	DATETIME2 DEFAULT GETDATE()
 );
+
+GO
